3ca7488c3fd6f071000caad5de7b5bcf4b607c96
andrei-kovalev-monopoly <168826136+andrei-kovalev-monopoly@users.noreply.github.com>
2024-05-06 17:23:28 +0300

    Update main.py
    Добавил счётчики

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from api_fields import hit_field_list, visit_field_list
 
 if __name__ == "__main__":
     token = os.getenv("TOKEN")
-    counter_id = os.getenv("COUNTER_ID")
+    counter_id1 = os.getenv("COUNTER_PLATFORM")
+    counter_id2 = os.getenv("COUNTER_CARGOSEARCH")  # Add another counter ID
     api_host_url = "https://api-metrika.yandex.ru"
 
     start_date = os.getenv("START_DATE")
@@ -20,17 +21,29 @@ if __name__ == "__main__":
     data_list = [{
         "source": "hits",
         "api_field_list": hit_field_list,
-        "google_sheet_url": os.getenv("HIT_SHEET_URL")
+        "google_sheet_url": os.getenv("HIT_SHEET_URL_PLATFORM"),
+        "counter_id": counter_id1  # Use the first counter ID
     }, {
         "source": "visits",
         "api_field_list": visit_field_list,
-        "google_sheet_url": os.getenv("VISIT_SHEET_URL")
+        "google_sheet_url": os.getenv("VISIT_SHEET_URL_PLATFORM"),
+        "counter_id": counter_id1  # Use the first counter ID
+    }, {
+        "source": "hits",
+        "api_field_list": hit_field_list,
+        "google_sheet_url": os.getenv("HIT_SHEET_URL_CARGOSEARCH"),  # Add another Google Sheet URL
+        "counter_id": counter_id2  # Use the second counter ID
+    }, {
+        "source": "visits",
+        "api_field_list": visit_field_list,
+        "google_sheet_url": os.getenv("VISIT_SHEET_URL_CARGOSEARCH"),  # Add another Google Sheet URL
+        "counter_id": counter_id2  # Use the second counter ID
     }]
     gc = service_account()
 
     for data_elem in data_list:
         data = get_log_data(api_host_url,
-                            counter_id,
+                            data_elem["counter_id"],  # Use the counter ID from the data element
                             token,
                             data_elem["source"],
                             start_date,
