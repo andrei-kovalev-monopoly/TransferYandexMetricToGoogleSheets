26bb9ca615aa348926dfa780eeb1b987c6cd5649
andrei-kovalev-monopoly <168826136+andrei-kovalev-monopoly@users.noreply.github.com>
2024-05-06 18:23:33 +0300

    Update main.py
    Правки работоспособности выгрузки нескольких счётчиков

--- main.py
+++ main.py
@@ -18,32 +18,44 @@ if __name__ == "__main__":
     yesterday = (date.today() - relativedelta(days=1)).strftime("%Y-%m-%d")
     end_date = os.getenv("END_DATE", default=yesterday)
 
-    data_list = [{
+    data_list_platform = [{
         "source": "hits",
         "api_field_list": hit_field_list,
-        "google_sheet_url": os.getenv("HIT_SHEET_URL_PLATFORM"),
-        "counter_id": counter_id1  # Use the first counter ID
+        "google_sheet_url": os.getenv("HIT_SHEET_URL_PLATFORM")
     }, {
         "source": "visits",
         "api_field_list": visit_field_list,
-        "google_sheet_url": os.getenv("VISIT_SHEET_URL_PLATFORM"),
-        "counter_id": counter_id1  # Use the first counter ID
-    }, {
-        "source": "hits",
-        "api_field_list": hit_field_list,
-        "google_sheet_url": os.getenv("HIT_SHEET_URL_CARGOSEARCH"),  # Add another Google Sheet URL
-        "counter_id": counter_id2  # Use the second counter ID
-    }, {
-        "source": "visits",
-        "api_field_list": visit_field_list,
-        "google_sheet_url": os.getenv("VISIT_SHEET_URL_CARGOSEARCH"),  # Add another Google Sheet URL
-        "counter_id": counter_id2  # Use the second counter ID
+        "google_sheet_url": os.getenv("VISIT_SHEET_URL_PLATFORM")
     }]
     gc = service_account()
 
-    for data_elem in data_list:
+    for data_elem in data_list_platform:
         data = get_log_data(api_host_url,
-                            data_elem["counter_id"],  # Use the counter ID from the data element
+                            counter_id,
+                            token,
+                            data_elem["source"],
+                            start_date,
+                            end_date,
+                            data_elem["api_field_list"])
+
+        sh = gc.open_by_url(data_elem["google_sheet_url"])
+        sh.sheet1.update([data.columns.values.tolist()]
+                         + data.fillna("Unknown").values.tolist())
+        
+    data_list_cargosearch= [{
+        "source": "hits",
+        "api_field_list": hit_field_list,
+        "google_sheet_url": os.getenv("HIT_SHEET_URL_CARGOSEARCH")
+    }, {
+        "source": "visits",
+        "api_field_list": visit_field_list,
+        "google_sheet_url": os.getenv("VISIT_SHEET_URL_CARGOSEARCH")
+    }]
+    gc = service_account()
+
+    for data_elem in data_list_cargosearch:
+        data = get_log_data(api_host_url,
+                            counter_id,
                             token,
                             data_elem["source"],
                             start_date,
